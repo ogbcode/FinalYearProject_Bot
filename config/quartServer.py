5f6c4baeed1087ca5c0db60b7e4d8ab2bb04eb0e
OGB <chidubem.info@gmail.com>
2024-05-10 15:46:40 +0100

    update

--- config/quartServer.py
+++ config/quartServer.py
@@ -1,5 +1,5 @@
 
-backend_url="http://telebotsolutions.railway.internal:6630"
+backend_url="https://telebotsolutions.up.railway.app"
 # backend_url="http://[::1]:3000"
 from quart import Quart
 app = Quart(__name__)
